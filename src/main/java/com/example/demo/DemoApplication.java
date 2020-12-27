326f927a83a671b4aa56426c7ae061ac4b2af0f7
Daniil Muntyan <daniilmuntjan@gmail.com>
2020-12-27 22:09:29 +0200

    .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,11 +8,13 @@ import com.example.demo.service.RoleService;
 import com.example.demo.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import java.util.Collections;
@@ -23,6 +25,11 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean(name="entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        return new LocalSessionFactoryBean();
     }
 
     @Bean
