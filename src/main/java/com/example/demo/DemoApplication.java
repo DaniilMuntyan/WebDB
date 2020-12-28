64a5c670463f3e83870ea3e3e1bee6da2ae0b429
Daniil Muntyan <daniilmuntjan@gmail.com>
2020-12-28 06:44:55 +0200

    .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Collections;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.demo.repositories")
 public class DemoApplication {
 
     public static void main(String[] args) {
